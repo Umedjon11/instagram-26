52d393cae6cdaeda94e75f7945c91664aaa4b85b
Umedjon <nazarovumed828@gmail.com>
2026-01-20 18:11:27 +0500

    part of sidebar + login

--- components/sideBar.tsx
+++ components/sideBar.tsx
@@ -1,18 +1,100 @@
 "use client"
 
+import { getUserProfileInfo } from "@/apis/user/profile"
 import { axiosRequest, isLogined } from "@/utils/axios"
+import Link from "next/link"
 import { usePathname } from "next/navigation"
+import { useEffect, useState } from "react"
+import InstagramTextLogo from "./instagramTextLogo"
+import { InstagramIcon } from "lucide-react"
 
 const SideBar = () => {
   const pathname = usePathname()
+  const [openSearch, setOpenSearch] = useState(false)
+  const [userProfile, setUserProfile] = useState<object | any>({})
 
+  useEffect(() => {
+    const getProfile = async () => {
+      const profile = await getUserProfileInfo()
+      if (profile) {
+        if (pathname == "/login" || pathname == "/register") {
+          window.location.pathname = "/"
+        }
+        setUserProfile(profile)
+      }
+    }
+
+    getProfile()
+  }, [])
 
   if (pathname == "/login" || pathname == "/register") {
     return null
   }
 
   return (
-    <div className="pr-4 h-screen w-[18%] max-h-screen fixed bg-white dark:bg-black border-r-2">
+    <div className={`pr-4 transition-all duration-400 h-screen ${pathname == "/messages" || openSearch ? "w-[5.5%] pl-5" : "w-[18%] pl-6"} border max-h-screen pt-[4vh] flex flex-col gap-[1vh] fixed bg-white dark:bg-black`}>
+      <div className={openSearch || pathname == "/messages" ? "hidden" : "block mb-[4vh]"}><InstagramTextLogo width="153" height="50" /></div>
+      <div className={!openSearch && pathname != "/messages" ? "hidden" : "mb-[5vh] m-[0_auto]"}><InstagramIcon size={30} /></div>
+      <Link onClick={() => setOpenSearch(false)} className={`flex ${pathname == "/" ? "font-bold" : "font-semibold"} ${pathname == "/messages" || openSearch ? "p-[1.5vh]" : "p-[1.7vh_20px]"} transition-all duration-300 rounded-md w-full hover:bg-[#F3F3F3] dark:hover:bg-[#25282C] gap-3 items-center`} href="/">
+        {!openSearch && pathname == "/" ?
+          (<svg aria-label="Home" className="x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Home</title><path d="m21.762 8.786-7-6.68a3.994 3.994 0 0 0-5.524 0l-7 6.681A4.017 4.017 0 0 0 1 11.68V19c0 2.206 1.794 4 4 4h3.005a1 1 0 0 0 1-1v-7.003a2.997 2.997 0 0 1 5.994 0V22a1 1 0 0 0 1 1H19c2.206 0 4-1.794 4-4v-7.32a4.02 4.02 0 0 0-1.238-2.894Z"></path></svg>) :
+          (<svg aria-label="Home" className="x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Home</title><path d="m21.762 8.786-7-6.68C13.266.68 10.734.68 9.238 2.106l-7 6.681A4.017 4.017 0 0 0 1 11.68V20c0 1.654 1.346 3 3 3h5.005a1 1 0 0 0 1-1L10 15c0-1.103.897-2 2-2 1.09 0 1.98.877 2 1.962L13.999 22a1 1 0 0 0 1 1H20c1.654 0 3-1.346 3-3v-8.32a4.021 4.021 0 0 0-1.238-2.894ZM21 20a1 1 0 0 1-1 1h-4.001L16 15c0-2.206-1.794-4-4-4s-4 1.794-4 4l.005 6H4a1 1 0 0 1-1-1v-8.32c0-.543.226-1.07.62-1.447l7-6.68c.747-.714 2.013-.714 2.76 0l7 6.68c.394.376.62.904.62 1.448V20Z"></path></svg>)}
+        <span className={openSearch || pathname == "/messages" ? "hidden" : ""}>Home</span>
+      </Link>
+      <button onClick={() => setOpenSearch(true)} className={`flex ${openSearch ? "font-bold" : "font-semibold"} ${pathname == "/messages" || openSearch ? "p-[1.5vh]" : "p-[1.7vh_20px]"} transition-all duration-300 rounded-md w-full cursor-pointer hover:bg-[#F3F3F3] dark:hover:bg-[#25282C] gap-3 items-center`}>
+        {!openSearch ?
+          (<svg aria-label="Search" className="x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Search</title><path d="M19 10.5A8.5 8.5 0 1 1 10.5 2a8.5 8.5 0 0 1 8.5 8.5Z" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"></path><line fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x1="16.511" x2="22" y1="16.511" y2="22"></line></svg>) :
+          (<svg aria-label="Search" className="x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Search</title><path d="M18.5 10.5a8 8 0 1 1-8-8 8 8 0 0 1 8 8Z" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3"></path><line fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" x1="16.511" x2="21.643" y1="16.511" y2="21.643"></line></svg>)}
+        <span className={openSearch || pathname == "/messages" ? "hidden" : ""}>Search</span>
+      </button>
+      <Link onClick={() => setOpenSearch(false)} className={`flex ${pathname == "/interested" ? "font-bold" : "font-semibold"} ${pathname == "/messages" || openSearch ? "p-[1.5vh]" : "p-[1.7vh_20px]"} transition-all duration-300 rounded-md w-full hover:bg-[#F3F3F3] dark:hover:bg-[#25282C] gap-3 items-center`} href="/interested">
+        {!openSearch && pathname == "/interested" ?
+          (<svg aria-label="Interested" className="x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Interested</title><path d="m13.173 13.164 1.491-3.829-3.83 1.49ZM12.001.5a11.5 11.5 0 1 0 11.5 11.5A11.513 11.513 0 0 0 12.001.5Zm5.35 7.443-2.478 6.369a1 1 0 0 1-.57.569l-6.36 2.47a1 1 0 0 1-1.294-1.294l2.48-6.369a1 1 0 0 1 .57-.569l6.359-2.47a1 1 0 0 1 1.294 1.294Z"></path></svg>) :
+          (<svg aria-label="Interested" className="x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Interested</title><polygon fill="none" points="13.941 13.953 7.581 16.424 10.06 10.056 16.42 7.585 13.941 13.953" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"></polygon><polygon fillRule="evenodd" points="10.06 10.056 13.949 13.945 7.581 16.424 10.06 10.056"></polygon><circle cx="12.001" cy="12.005" fill="none" r="10.5" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"></circle></svg>)}
+          <span className={openSearch || pathname == "/messages" ? "hidden" : ""}>Interested</span>
+      </Link>
+      <Link onClick={() => setOpenSearch(false)} className={`flex ${pathname == "/reels" ? "font-bold" : "font-semibold"} ${pathname == "/messages" || openSearch ? "p-[1.5vh]" : "p-[1.7vh_20px]"} transition-all duration-300 rounded-md w-full hover:bg-[#F3F3F3] dark:hover:bg-[#25282C] gap-3 items-center`} href="/reels">
+        {!openSearch && pathname == "/reels" ?
+          (<svg aria-label="Reels" className="x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Reels</title><path d="M22.942 7.464c-.062-1.36-.306-2.143-.511-2.671a5.366 5.366 0 0 0-1.272-1.952 5.364 5.364 0 0 0-1.951-1.27c-.53-.207-1.312-.45-2.673-.513-1.2-.054-1.557-.066-4.535-.066s-3.336.012-4.536.066c-1.36.062-2.143.306-2.672.511-.769.3-1.371.692-1.951 1.272s-.973 1.182-1.27 1.951c-.207.53-.45 1.312-.513 2.673C1.004 8.665.992 9.022.992 12s.012 3.336.066 4.536c.062 1.36.306 2.143.511 2.671.298.77.69 1.373 1.272 1.952.58.581 1.182.974 1.951 1.27.53.207 1.311.45 2.673.513 1.199.054 1.557.066 4.535.066s3.336-.012 4.536-.066c1.36-.062 2.143-.306 2.671-.511a5.368 5.368 0 0 0 1.953-1.273c.58-.58.972-1.181 1.27-1.95.206-.53.45-1.312.512-2.673.054-1.2.066-1.557.066-4.535s-.012-3.336-.066-4.536Zm-7.085 6.055-5.25 3c-1.167.667-2.619-.175-2.619-1.519V9c0-1.344 1.452-2.186 2.619-1.52l5.25 3c1.175.672 1.175 2.368 0 3.04Z"></path></svg>) :
+          (<svg aria-label="Reels" className="x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Reels</title><path d="M22.935 7.468c-.063-1.36-.307-2.142-.512-2.67a5.341 5.341 0 0 0-1.27-1.95 5.345 5.345 0 0 0-1.95-1.27c-.53-.206-1.311-.45-2.672-.513C15.333 1.012 14.976 1 12 1s-3.333.012-4.532.065c-1.36.063-2.142.307-2.67.512-.77.298-1.371.69-1.95 1.27a5.36 5.36 0 0 0-1.27 1.95c-.206.53-.45 1.311-.513 2.672C1.012 8.667 1 9.024 1 12s.012 3.333.065 4.532c.063 1.36.307 2.142.512 2.67.297.77.69 1.372 1.27 1.95.58.581 1.181.974 1.95 1.27.53.206 1.311.45 2.672.513C8.667 22.988 9.024 23 12 23s3.333-.012 4.532-.065c1.36-.063 2.142-.307 2.67-.512a5.33 5.33 0 0 0 1.95-1.27 5.356 5.356 0 0 0 1.27-1.95c.206-.53.45-1.311.513-2.672.053-1.198.065-1.555.065-4.531s-.012-3.333-.065-4.532Zm-1.998 8.972c-.05 1.07-.228 1.652-.38 2.04-.197.51-.434.874-.82 1.258a3.362 3.362 0 0 1-1.258.82c-.387.151-.97.33-2.038.379-1.162.052-1.51.063-4.441.063s-3.28-.01-4.44-.063c-1.07-.05-1.652-.228-2.04-.38a3.354 3.354 0 0 1-1.258-.82 3.362 3.362 0 0 1-.82-1.258c-.151-.387-.33-.97-.379-2.038C3.011 15.28 3 14.931 3 12s.01-3.28.063-4.44c.05-1.07.228-1.652.38-2.04.197-.51.434-.875.82-1.26a3.372 3.372 0 0 1 1.258-.819c.387-.15.97-.329 2.038-.378C8.72 3.011 9.069 3 12 3s3.28.01 4.44.063c1.07.05 1.652.228 2.04.38.51.197.874.433 1.258.82.385.382.622.747.82 1.258.151.387.33.97.379 2.038C20.989 8.72 21 9.069 21 12s-.01 3.28-.063 4.44Zm-4.584-6.828-5.25-3a2.725 2.725 0 0 0-2.745.01A2.722 2.722 0 0 0 6.988 9v6c0 .992.512 1.88 1.37 2.379.432.25.906.376 1.38.376.468 0 .937-.123 1.365-.367l5.25-3c.868-.496 1.385-1.389 1.385-2.388s-.517-1.892-1.385-2.388Zm-.993 3.04-5.25 3a.74.74 0 0 1-.748-.003.74.74 0 0 1-.374-.649V9a.74.74 0 0 1 .374-.65.737.737 0 0 1 .748-.002l5.25 3c.341.196.378.521.378.652s-.037.456-.378.651Z"></path></svg>)}
+        <span className={openSearch || pathname == "/messages" ? "hidden" : ""}>Reels</span>
+      </Link>
+      <Link onClick={() => setOpenSearch(false)} className={`flex ${pathname == "/messages" ? "font-bold" : "font-semibold"} ${pathname == "/messages" || openSearch ? "p-[1.5vh]" : "p-[1.7vh_20px]"} transition-all duration-300 rounded-md w-full hover:bg-[#F3F3F3] dark:hover:bg-[#25282C] gap-3 items-center`} href="/messages">
+        {!openSearch && pathname == "/messages" ?
+          (<svg aria-label="Сообщения" className="x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Messages</title><path d="M22.513 3.576C21.826 2.552 20.617 2 19.384 2H4.621c-1.474 0-2.878.818-3.46 2.173-.6 1.398-.297 2.935.784 3.997l3.359 3.295a1 1 0 0 0 1.195.156l8.522-4.849a1 1 0 1 1 .988 1.738l-8.526 4.851a1 1 0 0 0-.477 1.104l1.218 5.038c.343 1.418 1.487 2.534 2.927 2.766.208.034.412.051.616.051 1.26 0 2.401-.644 3.066-1.763l7.796-13.118a3.572 3.572 0 0 0-.116-3.863Z"></path></svg>) : 
+          (<svg aria-label="Сообщения" className="x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Messages</title><path d="M13.973 20.046 21.77 6.928C22.8 5.195 21.55 3 19.535 3H4.466C2.138 3 .984 5.825 2.646 7.456l4.842 4.752 1.723 7.121c.548 2.266 3.571 2.721 4.762.717Z" fill="none" stroke="currentColor" strokeLinejoin="round" strokeWidth="2"></path><line fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" stroke-width="2" x1="7.488" x2="15.515" y1="12.208" y2="7.641"></line></svg>)}
+        <span className={openSearch || pathname == "/messages" ? "hidden" : ""}>Messages</span>
+      </Link>
+      <Link onClick={() => setOpenSearch(false)} className={`flex ${pathname == "/" ? "font-bold" : "font-semibold"} ${pathname == "/messages" || openSearch ? "p-[1.5vh]" : "p-[1.7vh_20px]"} transition-all duration-300 rounded-md w-full hover:bg-[#F3F3F3] dark:hover:bg-[#25282C] gap-3 items-center`} href="/">
+        {!openSearch && pathname == "/" ?
+          (<svg aria-label="Главная" className="x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Home</title><path d="m21.762 8.786-7-6.68a3.994 3.994 0 0 0-5.524 0l-7 6.681A4.017 4.017 0 0 0 1 11.68V19c0 2.206 1.794 4 4 4h3.005a1 1 0 0 0 1-1v-7.003a2.997 2.997 0 0 1 5.994 0V22a1 1 0 0 0 1 1H19c2.206 0 4-1.794 4-4v-7.32a4.02 4.02 0 0 0-1.238-2.894Z"></path></svg>) : 
+          (<svg aria-label="Главная" className="x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Home</title><path d="m21.762 8.786-7-6.68C13.266.68 10.734.68 9.238 2.106l-7 6.681A4.017 4.017 0 0 0 1 11.68V20c0 1.654 1.346 3 3 3h5.005a1 1 0 0 0 1-1L10 15c0-1.103.897-2 2-2 1.09 0 1.98.877 2 1.962L13.999 22a1 1 0 0 0 1 1H20c1.654 0 3-1.346 3-3v-8.32a4.021 4.021 0 0 0-1.238-2.894ZM21 20a1 1 0 0 1-1 1h-4.001L16 15c0-2.206-1.794-4-4-4s-4 1.794-4 4l.005 6H4a1 1 0 0 1-1-1v-8.32c0-.543.226-1.07.62-1.447l7-6.68c.747-.714 2.013-.714 2.76 0l7 6.68c.394.376.62.904.62 1.448V20Z"></path></svg>)}
+        <span className={openSearch || pathname == "/messages" ? "hidden" : ""}>Home</span>
+      </Link>
+      <Link onClick={() => setOpenSearch(false)} className={`flex ${pathname == "/" ? "font-bold" : "font-semibold"} ${pathname == "/messages" || openSearch ? "p-[1.5vh]" : "p-[1.7vh_20px]"} transition-all duration-300 rounded-md w-full hover:bg-[#F3F3F3] dark:hover:bg-[#25282C] gap-3 items-center`} href="/">
+        {!openSearch && pathname == "/" ?
+          (<svg aria-label="Главная" className="x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Home</title><path d="m21.762 8.786-7-6.68a3.994 3.994 0 0 0-5.524 0l-7 6.681A4.017 4.017 0 0 0 1 11.68V19c0 2.206 1.794 4 4 4h3.005a1 1 0 0 0 1-1v-7.003a2.997 2.997 0 0 1 5.994 0V22a1 1 0 0 0 1 1H19c2.206 0 4-1.794 4-4v-7.32a4.02 4.02 0 0 0-1.238-2.894Z"></path></svg>) : 
+          (<svg aria-label="Главная" className="x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Home</title><path d="m21.762 8.786-7-6.68C13.266.68 10.734.68 9.238 2.106l-7 6.681A4.017 4.017 0 0 0 1 11.68V20c0 1.654 1.346 3 3 3h5.005a1 1 0 0 0 1-1L10 15c0-1.103.897-2 2-2 1.09 0 1.98.877 2 1.962L13.999 22a1 1 0 0 0 1 1H20c1.654 0 3-1.346 3-3v-8.32a4.021 4.021 0 0 0-1.238-2.894ZM21 20a1 1 0 0 1-1 1h-4.001L16 15c0-2.206-1.794-4-4-4s-4 1.794-4 4l.005 6H4a1 1 0 0 1-1-1v-8.32c0-.543.226-1.07.62-1.447l7-6.68c.747-.714 2.013-.714 2.76 0l7 6.68c.394.376.62.904.62 1.448V20Z"></path></svg>)}
+        <span className={openSearch || pathname == "/messages" ? "hidden" : ""}>Home</span>
+      </Link>
+      <Link onClick={() => setOpenSearch(false)} className={`flex ${pathname == "/" ? "font-bold" : "font-semibold"} ${pathname == "/messages" || openSearch ? "p-[1.5vh]" : "p-[1.7vh_20px]"} transition-all duration-300 rounded-md w-full hover:bg-[#F3F3F3] dark:hover:bg-[#25282C] gap-3 items-center`} href="/">
+        {!openSearch && pathname == "/" ?
+          (<svg aria-label="Главная" className="x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Home</title><path d="m21.762 8.786-7-6.68a3.994 3.994 0 0 0-5.524 0l-7 6.681A4.017 4.017 0 0 0 1 11.68V19c0 2.206 1.794 4 4 4h3.005a1 1 0 0 0 1-1v-7.003a2.997 2.997 0 0 1 5.994 0V22a1 1 0 0 0 1 1H19c2.206 0 4-1.794 4-4v-7.32a4.02 4.02 0 0 0-1.238-2.894Z"></path></svg>) : 
+          (<svg aria-label="Главная" className="x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Home</title><path d="m21.762 8.786-7-6.68C13.266.68 10.734.68 9.238 2.106l-7 6.681A4.017 4.017 0 0 0 1 11.68V20c0 1.654 1.346 3 3 3h5.005a1 1 0 0 0 1-1L10 15c0-1.103.897-2 2-2 1.09 0 1.98.877 2 1.962L13.999 22a1 1 0 0 0 1 1H20c1.654 0 3-1.346 3-3v-8.32a4.021 4.021 0 0 0-1.238-2.894ZM21 20a1 1 0 0 1-1 1h-4.001L16 15c0-2.206-1.794-4-4-4s-4 1.794-4 4l.005 6H4a1 1 0 0 1-1-1v-8.32c0-.543.226-1.07.62-1.447l7-6.68c.747-.714 2.013-.714 2.76 0l7 6.68c.394.376.62.904.62 1.448V20Z"></path></svg>)}
+        <span className={openSearch || pathname == "/messages" ? "hidden" : ""}>Home</span>
+      </Link>
+      <Link onClick={() => setOpenSearch(false)} className={`flex ${pathname == "/" ? "font-bold" : "font-semibold"} ${pathname == "/messages" || openSearch ? "p-[1.5vh]" : "p-[1.7vh_20px]"} mt-auto transition-all duration-300 rounded-md w-full hover:bg-[#F3F3F3] dark:hover:bg-[#25282C] gap-3 items-center`} href="/">
+        {!openSearch && pathname == "/" ?
+          (<svg aria-label="Главная" className="x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Home</title><path d="m21.762 8.786-7-6.68a3.994 3.994 0 0 0-5.524 0l-7 6.681A4.017 4.017 0 0 0 1 11.68V19c0 2.206 1.794 4 4 4h3.005a1 1 0 0 0 1-1v-7.003a2.997 2.997 0 0 1 5.994 0V22a1 1 0 0 0 1 1H19c2.206 0 4-1.794 4-4v-7.32a4.02 4.02 0 0 0-1.238-2.894Z"></path></svg>) : 
+          (<svg aria-label="Главная" className="x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Home</title><path d="m21.762 8.786-7-6.68C13.266.68 10.734.68 9.238 2.106l-7 6.681A4.017 4.017 0 0 0 1 11.68V20c0 1.654 1.346 3 3 3h5.005a1 1 0 0 0 1-1L10 15c0-1.103.897-2 2-2 1.09 0 1.98.877 2 1.962L13.999 22a1 1 0 0 0 1 1H20c1.654 0 3-1.346 3-3v-8.32a4.021 4.021 0 0 0-1.238-2.894ZM21 20a1 1 0 0 1-1 1h-4.001L16 15c0-2.206-1.794-4-4-4s-4 1.794-4 4l.005 6H4a1 1 0 0 1-1-1v-8.32c0-.543.226-1.07.62-1.447l7-6.68c.747-.714 2.013-.714 2.76 0l7 6.68c.394.376.62.904.62 1.448V20Z"></path></svg>)}
+        <span className={openSearch || pathname == "/messages" ? "hidden" : ""}>Home</span>
+      </Link>
+      <Link onClick={() => setOpenSearch(false)} className={`flex ${pathname == "/" ? "font-bold" : "font-semibold"} mb-[3vh] ${pathname == "/messages" || openSearch ? "p-[1.5vh]" : "p-[1.7vh_20px]"} transition-all duration-300 rounded-md w-full hover:bg-[#F3F3F3] dark:hover:bg-[#25282C] gap-3 items-center`} href="/">
+        {openSearch || pathname == "/" ?
+          (<svg aria-label="Главная" className="x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Home</title><path d="m21.762 8.786-7-6.68a3.994 3.994 0 0 0-5.524 0l-7 6.681A4.017 4.017 0 0 0 1 11.68V19c0 2.206 1.794 4 4 4h3.005a1 1 0 0 0 1-1v-7.003a2.997 2.997 0 0 1 5.994 0V22a1 1 0 0 0 1 1H19c2.206 0 4-1.794 4-4v-7.32a4.02 4.02 0 0 0-1.238-2.894Z"></path></svg>) : 
+          (<svg aria-label="Главная" className="x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Home</title><path d="m21.762 8.786-7-6.68C13.266.68 10.734.68 9.238 2.106l-7 6.681A4.017 4.017 0 0 0 1 11.68V20c0 1.654 1.346 3 3 3h5.005a1 1 0 0 0 1-1L10 15c0-1.103.897-2 2-2 1.09 0 1.98.877 2 1.962L13.999 22a1 1 0 0 0 1 1H20c1.654 0 3-1.346 3-3v-8.32a4.021 4.021 0 0 0-1.238-2.894ZM21 20a1 1 0 0 1-1 1h-4.001L16 15c0-2.206-1.794-4-4-4s-4 1.794-4 4l.005 6H4a1 1 0 0 1-1-1v-8.32c0-.543.226-1.07.62-1.447l7-6.68c.747-.714 2.013-.714 2.76 0l7 6.68c.394.376.62.904.62 1.448V20Z"></path></svg>)}
+        <span className={openSearch || pathname == "/messages" ? "hidden" : ""}>Home</span>
+      </Link>
     </div>
   )
 }
