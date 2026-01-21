04c5a08da13654fe1a073ff8783c8f52f7e37023
rama-roma <cospleyergirl@gmail.com>
2026-01-21 19:03:22 +0500

    psapsa

--- reducers/mesage.ts
+++ reducers/mesage.ts
@@ -5,7 +5,8 @@ const initialState = {
   chats: [],
   loading: false,
   error: null,
-  chatById: null,
+  chatById: [],
+  messages: [],
 };
 
 export const getChats = createAsyncThunk("chats/getChats", async () => {
@@ -47,28 +48,47 @@ export const deleteChatById = createAsyncThunk(
   },
 );
 
+export const sendChatMessage = createAsyncThunk(
+  "chats/sendChatMessage",
+  async ({ chatId, message }: { chatId: number; message: string }) => {
+    try {
+      const formData = new FormData();
+      formData.append("ChatId", String(chatId));
+      formData.append("MessageText", message);         
+
+      const { data } = await axiosRequest.put("/Chat/send-message", formData);
+      return data;
+    } catch (error) {
+      isLogined(error);
+      console.error(error);
+    }
+  }
+);
+
+
 export const chatsSlice = createSlice({
   name: "chats",
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder
-      .addCase(getChats.pending, (state) => {
-        state.loading = true;
-        state.error = null;
-      })
-      .addCase(getChats.fulfilled, (state, action) => {
-        state.loading = false;
-        state.chats = action.payload;
-      })
-      .addCase(getChatById.pending, (state) => {
-        state.loading = true;
-      })
-      .addCase(getChatById.fulfilled, (state, action) => {
-        state.loading = false;
-        state.chatById = action.payload;
-      });
-  },
+  builder
+    .addCase(getChats.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    })
+    .addCase(getChats.fulfilled, (state, action) => {
+      state.loading = false;
+      state.chats = action.payload;
+    })
+    .addCase(getChatById.pending, (state) => {
+      state.loading = true;
+    })
+    .addCase(getChatById.fulfilled, (state, action) => {
+      state.loading = false;
+      state.chatById = action.payload.data;
+    })
+  
+}
 });
 
 export default chatsSlice.reducer;
