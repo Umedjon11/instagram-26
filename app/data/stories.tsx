1e586c8ad4cf6f1ffab4ccf7d8810059ef1f2c61
fozilov-16 <fazylovmuhammad1606@gmail.com>
2026-01-21 18:10:41 +0500

    Home Page

---
+++ app/data/stories.tsx
@@ -0,0 +1,18 @@
+export const stories = [
+  {
+    url: "/women.jpg",
+    duration: 3000,
+  },
+  {
+    url: "/women.jpg",
+    duration: 3000,
+  },
+  {
+    content: () => (
+      <div className="w-full h-full bg-black flex items-center justify-center text-white text-3xl">
+        Hello 👋
+      </div>
+    ),
+    duration: 4000,
+  },
+];
